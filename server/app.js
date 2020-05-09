f91a35e36ec48d868a5f4cca53954b970b570ff7
amitlzkpa <amit.lzkpa@gmail.com>
2020-05-09 16:39:49 -0500

    use index file for home page; static folder setup; use port 4000

--- server/app.js
+++ server/app.js
@@ -2,17 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT || 8080;
+const DIR = 'server/src';
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static(DIR));
 
-app.get('/', function(req, res) {
-  console.log('foo');
-  res.send('bar');
-});
+// app.get('/', function(req, res) {
+//   console.log('foo');
+//   res.send('bar');
+// });
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
